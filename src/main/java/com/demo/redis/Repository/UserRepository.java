c4a9acececfbab0b42d4f0bd19e8dac490e2de54
vemuri <vemuri.nath@oyorooms.com>
2020-09-25 10:23:52 +0530

    added search

--- src/main/java/com/demo/redis/Repository/UserRepository.java
+++ src/main/java/com/demo/redis/Repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.demo.redis.Repository;
 
 import com.demo.redis.Model.Users;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -26,6 +27,7 @@ public class UserRepository {
     //save the user
     public void save(Users users){
         hashOperations.put("USER", users.getId().toString(),users);
+        hashOperations.put("USER_MAIL",users.getEmail(),users);
     }
 
     //get by user id
@@ -33,14 +35,22 @@ public class UserRepository {
         return (Users) hashOperations.get("USER",id.toString());
     }
 
+    //get by user Email
+    public Users getByEmail(String email){
+        return (Users) hashOperations.get("USER_MAIL",email);
+    }
+
     //save all users
     public void saveAll(List<Users>list){
         Map<String, Object> map1 = new HashMap<>();
+        Map<String, Object>map2 = new HashMap<>();
         for (Users u:
              list) {
             map1.put(u.getId().toString(), u);
+            map2.put(u.getEmail(), u);
         }
         hashOperations.putAll("USER",map1);
+        hashOperations.putAll("USER_MAIL",map2);
     }
 
     //get the list of all users
